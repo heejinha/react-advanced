161de0340b29f51112b16ae3129abd0fcb2ddbe8
hjha <hjha@gaia3d.com>
2023-11-15 14:10:02 +0900

    chore: 라우터 사용을 위해 historyApiFallback 설정 추가
    - historyApiFallback = true 설정 시, [path].html 없는 경우 index.html 리턴

--- shared/webpack.config.js
+++ shared/webpack.config.js
@@ -1,32 +1,35 @@
 const path = require("path");
 
 module.exports = {
-  mode: "development",
-  entry: {
-    main: "./src/main.js",
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].bundle.js",
-    clean: true,
-  },
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
-  devtool: "source-map",
-  devServer: {
-    static: path.resolve(__dirname, "public"),
-    port: process.env.PORT,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-    ],
-  },
+	mode: "development",
+	entry: {
+		main: "./src/main.js",
+	},
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "[name].bundle.js",
+		clean: true,
+	},
+	resolve: {
+		extensions: [ ".js", ".jsx" ],
+	},
+	devtool: "source-map",
+	devServer: {
+		static: path.resolve(__dirname, "public"),
+		port: process.env.PORT,
+		// historyApiFallback: false ==> /public 하위의 [path].html 리턴하고
+		// historyApiFallback: true ==> [path].html 없는 경우 index.html 로 결과 응답
+		historyApiFallback: true
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+				},
+			},
+		],
+	},
 };
