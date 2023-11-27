8e5bb16c7c0b4ff3d3abd93e086b95897cbdd459
hjha <hjha@gaia3d.com>
2023-11-27 19:19:33 +0900

    feat: 고차컴포넌트 예제 추가

--- 1-react/src/App.jsx
+++ 1-react/src/App.jsx
@@ -4,6 +4,7 @@ import { Router, Routes, Route } from './lib/MyRouter';
 import CartPage from './pages/CartPage';
 import OrderPage from './pages/OrderPage';
 import ProductPage from './pages/ProductPage';
+import { getComponentName } from './lib/utils';
 
 const App = () => {
 	return (
@@ -17,7 +18,58 @@ const App = () => {
 	);
 };
 
-export default App;
+// export default App;
+
+
+class Header extends Component {
+	render() {
+		return (
+			<header>Header</header>
+		);
+	}
+}
+
+class Button extends Component {
+	handleClick= () => {
+		this.props.log('클릭');
+	}
+
+	render() {
+		return (
+			<button onClick={this.handleClick}>button</button>
+		);
+	}
+}
+
+const withLogging = WrappedComponent => {
+	const log = (message) => {
+		console.log(`[${getComponentName(WrappedComponent)}] ${message}`);
+	}
+	class WithLogging extends Component {
+		render() {
+			const enhancedProps = {
+				log
+			}
+			return <WrappedComponent {...this.props} {...enhancedProps} />
+		}
+
+		componentDidMount() {
+			log('mounted');
+		}
+	}
+
+	return WithLogging;
+}
+
+const EnhancedHeader = withLogging(Header);
+const EnhancedButton = withLogging(Button);
+
+export default () => (
+	<>
+		<EnhancedHeader />
+		<EnhancedButton />
+	</>
+);
 
 
 
