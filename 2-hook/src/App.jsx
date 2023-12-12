f6e578a1ce6bf7e54704c89e33640ead979ece92
hjha <hjha@gaia3d.com>
2023-12-12 13:39:20 +0900

    feat: class 와 함수 컴포넌트 차이 관련 코드 추가

--- 2-hook/src/App.jsx
+++ 2-hook/src/App.jsx
@@ -1,3 +1,55 @@
-const App = () => <>2-hook</>;
+import { Component } from 'react';
 
+// const App = () => <>2-hook</>;
+
+// export default App;
+
+function Contract(props) {
+	const sign = () => {
+		setTimeout(() => console.log(props.name), 3000);
+	}
+	return <button onClick={sign}>sign</button>
+}
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: 'user1'
+		}
+	}
+	handleChange(e) {
+		this.setState({
+			name: e.target.value
+		})
+	}
+
+	render() {
+		return (
+			<>
+				<select value={this.state.name} onChange={this.handleChange.bind(this)}>
+					<option value="user1">user1</option>
+					<option value="user2">user2</option>
+				</select>
+				<Contract name={this.state.name} />
+			</>
+		);
+	}
+}
 export default App;
+
+// const contract = new Contract('user1');
+// contract.sign();
+// contract.name = 'user 2';
+//
+// function createContract(name) {
+// 	const sign = () => {
+// 		setTimeout(() => console.log(name), 3000);
+// 	}
+// 	return {
+// 		sign
+// 	}
+// }
+//
+// const contract2 = createContract('user3');
+// contract2.sign();
