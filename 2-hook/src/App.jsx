49d65251231652212a43992541e2f51bf0fe5097
hjha <hjha@gaia3d.com>
2024-01-23 22:35:28 +0900

    feat: 회원가입 폼 예제 기능 추가
    - 4.3.1 회원가입 폼 예제 수강 완료

--- 2-hook/src/App.jsx
+++ 2-hook/src/App.jsx
@@ -5,6 +5,7 @@ import button from './components/Button';
 import { Route, Router, Routes } from './lib/MyRouter';
 import { Layout } from './lib/MyLayout';
 import { ErrorMessage, Field, Form, useForm } from './lib/MyForm';
+import { useState } from 'react';
 
 const App = () => (
 	<>
@@ -20,7 +21,84 @@ const App = () => (
 	</>
 );
 
-export default App;
+// export default App;
+
+const useRegisterForm = () => {
+	const [state, setState] = useState({
+		value: { nickname: '', password: ''},
+		error: { nickname: '', password: ''}
+	});
+
+	const handleChange = ({ target }) => {
+		setState({
+			...state,
+			value: {
+				...state.value,
+				[target.name]: target.value
+			}
+		})
+
+	};
+
+	const handleReset = () => {
+		setState({
+			value: { nickname: '', password: ''},
+			error: { nickname: '', password: ''}
+		});
+	};
+
+	const handleSubmit = ({ target }) => {
+		setState({
+			...state,
+			error: {
+				nickname: /^\w+$/.test(state.value.nickname) ? '' : '영문, 숫자만 입력',
+				password: /^.{3,6}$/.test(state.value.password) ? '' : '3~6글자 입력'
+			}
+		})
+
+	};
+
+	return {
+		state,
+		handleChange,
+		handleReset,
+		handleSubmit
+	};
+};
+
+const RegisterForm = () => {
+	const { state, handleChange, handleReset, handleSubmit} = useRegisterForm();
+
+	return (
+		<>
+			<div>
+				<label htmlFor="">닉네임 : </label>
+				<input
+					type="text"
+					name="nickname"
+					value={state.value.nickname}
+					onChange={handleChange}
+				/>
+				<span>{state.error.nickname}</span>
+			</div>
+			<div>
+				<label htmlFor="">비밀번호 : </label>
+				<input
+					type="text"
+					name="password"
+					value={state.value.password}
+					onChange={handleChange}
+				/>
+				<span>{state.error.password}</span>
+			</div>
+			<button onClick={handleReset}>초기화</button>
+			<button onClick={handleSubmit}>회원가입</button>
+		</>
+	);
+
+};
+
+export default RegisterForm;
 
 
 // export default () => {
